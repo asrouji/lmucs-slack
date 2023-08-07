4397208dce7ff940b9124aefc4afbab639d0ca28
Aidan Srouji <72050533+asrouji@users.noreply.github.com>
2023-08-06 20:36:11 -0700

    set status 200 for challenge request

--- api/events.ts
+++ api/events.ts
@@ -1,11 +1,16 @@
 export default async function events(
   req: { body: { type: string; challenge: string } },
-  res: { send: (message: string) => void },
+  res: {
+    send: (message: string) => void
+    status: (code: number) => { send: (message: { challenge: string }) => void }
+  },
 ) {
   const type = req.body.type
 
   if (type == 'url_verification') {
-    res.send(req.body.challenge)
+    res.status(200).send({
+      challenge: req.body.challenge,
+    })
   } else if (type == 'event_callback') {
     console.log(req.body)
   }
