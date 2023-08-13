3ed4cdb50e63d99f038883b0bf6dcc20b6f923b7
Aidan Srouji <72050533+asrouji@users.noreply.github.com>
2023-08-12 20:03:08 -0700

    Versioning untracked files

---
+++ scripts/getCalendar.ts
@@ -0,0 +1,18 @@
+import { DateTime } from 'luxon'
+import { updateTutoringSessionDescription } from '../util/calendar'
+
+const date = DateTime.fromObject(
+  {
+    year: 2023,
+    month: 8,
+    day: 7,
+    hour: 16,
+  },
+  {
+    zone: 'America/Los_Angeles',
+  },
+)
+
+const res = await updateTutoringSessionDescription('Aidan S', 'hello world!', date)
+
+console.log(res ? `Updated event "${res.summary}"` : 'No event found')
