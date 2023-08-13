9b8015b10f93f65f37b4b0b68e14b896a55ec273
Aidan Srouji <72050533+asrouji@users.noreply.github.com>
2023-08-12 18:00:10 -0700

    attempt to get name of the user who sent the message

--- api/events.ts
+++ api/events.ts
@@ -10,6 +10,27 @@ export default async function events(req: SlackRequest, res: SlackResponse) {
     })
   } else if (type == 'event_callback') {
     res.status(200).send()
-    console.log(req.body)
+    if (req.body.event.type === 'message') {
+      const message = req.body.event.text
+      const userId = req.body.event.user
+
+      const response = await fetch(
+        `https://slack.com/api/users.info?token=${process.env.SLACK_BOT_TOKEN}&user=${userId}&pretty=1`,
+      )
+
+      const data = (await response.json()) as {
+        ok: boolean
+        user: {
+          profile: {
+            first_name: string
+            last_name: string
+          }
+        }
+      }
+
+      const name = `${data.user.profile.first_name} ${data.user.profile.last_name.slice(0, 1)}`
+
+      console.log(`Message from "${name}": "${message}"`)
+    }
   }
 }
